628f7a3425881c019afe4675a7ca07ef95755958
chrisachoo <mabs.chris@outlook.com>
2022-08-16 08:11:46 +0200

    update profile

--- smart/src/components/pages/onboarding/personal.jsx
+++ smart/src/components/pages/onboarding/personal.jsx
@@ -1,25 +1,27 @@
 import { useState, useNavigate, useEffect } from 'react'
-import { useAuthContext } from '../../hooks/useAuthContext'
+import { useUpdate } from '../../hooks/useUpdate'
 import { Input } from '../../index'
 
 const Personal = () => {
 
+  const { updateUser, isLoading, error } = useUpdate()
   const [form, setForm] = useState({
     first_name: '',
     last_name: '',
-    cellno: '',
     email: '',
+    cellno: '',
     token: ''
   })
+
   useEffect(() => {
     const fetchUser = async () => {
       const user = await JSON.parse(localStorage.getItem('user'))
-      if (user) { 
+      if (user) {
         setForm({
           first_name: user.first_name,
           last_name: user.last_name,
-          cellno: user.cellno,
           email: user.email,
+          cellno: user.cellno,
           token: user.token
         })
       }
@@ -36,58 +38,63 @@ const Personal = () => {
   }
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    console.log(form)
+    let { first_name, last_name, email, cellno, token } = form
+
+    console.log(`firstName: ${first_name}, lastName: ${last_name}, email: ${email}, cellno: ${cellno}, token: ${token}`)
+    await updateUser(first_name, last_name, email, cellno, token)
   }
 
   return (
     <div className='section__padding'>
       <h4>Personal Details</h4>
 
-      <Input
-        label={`First Name`}
-        type={`text`}
-        name={`first_name`}
-        value={form.first_name}
-        onChange={handleFormChange}
-        defaultValue={form.first_name}
-        placeholder={`First Name`}
-      />
+      <form onSubmit={handleSubmit}>
+        <Input
+          label={`First Name`}
+          type={`text`}
+          name={`first_name`}
+          value={form.first_name}
+          onChange={handleFormChange}
+          defaultValue={form.first_name}
+          placeholder={`First Name`}
+        />
 
-      <Input
-        label={`Last Name`}
-        type={`text`}
-        name={`last_name`}
-        value={form.last_name}
-        onChange={handleFormChange}
-        defaultValue={form.last_name}
-        placeholder={`First Name`}
-      />
+        <Input
+          label={`Last Name`}
+          type={`text`}
+          name={`last_name`}
+          value={form.last_name}
+          onChange={handleFormChange}
+          defaultValue={form.last_name}
+          placeholder={`First Name`}
+        />
 
-      <Input
-        label={`Contact Number`}
-        type={`text`}
-        name={`cellno`}
-        value={form.cellno}
-        onChange={handleFormChange}
-        defaultValue={form.cellno}
-        placeholder={`First Name`}
-      />
+        <Input
+          label={`Contact Number`}
+          type={`text`}
+          name={`cellno`}
+          value={form.cellno}
+          onChange={handleFormChange}
+          defaultValue={form.cellno}
+          placeholder={`First Name`}
+        />
 
-      <Input
-        label={`Email`}
-        type={`email`}
-        name={`email`}
-        value={form.email}
-        onChange={handleFormChange}
-        defaultValue={form.email}
-        placeholder={`First Name`}
-      />
-      <div className='group__content'>
-        <button className='btn btn-primary'>Cancel</button>
-        <button className='btn btn-secondary' onClick={() => console.log('form: ', form)}>Save</button>
-      </div>
+        <Input
+          label={`Email`}
+          type={`email`}
+          name={`email`}
+          value={form.email}
+          onChange={handleFormChange}
+          defaultValue={form.email}
+          placeholder={`First Name`}
+        />
+        <div className='group__content'>
+          <button className='btn btn-primary'>Cancel</button>
+          <button className='btn btn-secondary' type='submit'>Save</button>
+        </div>
+      </form>
     </div>
   )
 }
